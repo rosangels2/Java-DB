49fe6584ee792715314b9ece715a9f5cdc0135f0
github <rosangels2@naver.com>
2019-03-29 17:13:44 +0900

    100이하의 소수 출력 예제

--- project/src/project/day4.java
+++ project/src/project/day4.java
@@ -183,7 +183,88 @@ public class day4 {
 	*/	
 		
 		
-		//*****
+	/*	문제) 다음을 출력하시오
+		
+		 ********* 	i=1 *=9   공백=0
+		  ******* 	i=2 *=7   공백=1
+		   *****	i=3 *=5   공백=2
+		    ***	    i=4 *=3   공백=3
+	   	     *     	i=5 *=1   공백=4
+					              
+	*/	
+		
+		
+	/*	int i = 0, j = 0;
+		for(i=1; i<=6; i++){
+			for(j=1; j<=i-1; j++){
+				System.out.print(" ");
+			}
+			for(j=9; j>=2*i-1; j--){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	*/	
+		
+	
+		
+		//예제) 정수를 입력받아 입력받은 정수가 소수인지 아닌지 판별하는 코드를 반복문을 이용하여 작성하시오
+		//소수 : 약수가 1과 자기 자신인 수(약수가 2개인 수)
+		//약수 : 나누어 떨어지는 수 (4의 약수 = 1, 2, 4)
+		
+	/*	int num, i, cnt;
+		Scanner scan = new Scanner(System.in);
+		num = scan.nextInt();
+		for(i=1, cnt=0 ;i<=num ; i++){
+			//i가 num의 약수이면 약수의 갯수를 하나 증가
+			//num를 i로 나누었을 때 나머지가 0과 같다 => i가 num의 약수
+						if(num % i == 0){
+				cnt++;//ctn +=1 //cnt+1; //++cnt
+			}
+		}
+		//약수의 갯수가 2개이면 소수라고 출력
+		if(cnt == 2){
+			System.out.println(num+"는 소수");
+		}
+		//2개가 아니면 소수가 아님이라고 출력
+		else{
+			System.out.println(num+"는 소수가 아님");
+		}
+	*/	
+			
+			
+		
+		//100이하의 소수를 모두 출력하는 코드를 작성하시오
+		
+	/*	int num = 0;
+		int i, cnt=0;
+		for(num=1; num<=100; num++){
+			for(i=1,cnt=0; i<=num; i++){
+				if(num % i == 0){
+					cnt++;
+				}
+			}
+			if(cnt == 2){
+				System.out.print(num+" "); //가로로 출력
+			}
+				
+		}
+	*/	
+		
+	/*	int num = 0;
+		int i, cnt=0;
+		for(num=1; num<=100; num++){
+			for(i=1,cnt=0; i<=num; i++){
+				if(num % i == 0){
+					cnt++;
+				}
+			}
+			if(cnt == 2){
+				System.out.println(num); //세로로 출력
+			}
+				
+		}
+	*/	
 		
 		
 		
@@ -192,10 +273,22 @@ public class day4 {
 		
 		
 		
-	}
+		
+		
+		
+		
+			
+			
+		
+		
+		
+	
 
+
+     	}
+	
+	
 }
-
 
 
 
---
+++ project/src/project/day5.java
@@ -0,0 +1,47 @@
+package project;
+
+import java.util.Scanner;
+
+public class day5 {
+
+	public static void main(String[] args) {
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
