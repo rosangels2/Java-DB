8b46a22af41f854c2cd427db2978709846176a18
github <rosangels2@naver.com>
2019-04-01 16:55:57 +0900

    5일차

---
+++ project/src/project/day6.java
@@ -0,0 +1,32 @@
+package project;
+
+import java.util.Scanner;
+
+public class day6 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
