5817e83350f729be3e312d0e6f267b93b510b003
CJY <rosangels2@naver.com>
2019-06-17 17:11:04 +0900

    stopWatch / pagination

---
+++ project/src/practice/practice_calculator.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class practice_calculator {
+
+	public static void main(String[] args) {
+
+		int num = 12345;
+		int num1 = 100;
+		int sum = num % num1;
+		System.out.println(sum);
+	}
+
+}
