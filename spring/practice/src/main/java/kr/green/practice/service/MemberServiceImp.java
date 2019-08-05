dc99757d280c8735a88c0c3d5e4efe03702a555c
CJY <rosangels2@naver.com>
2019-08-05 13:41:27 +0900

    비밀번호 암호화 복습 및 재정리

--- spring/practice/src/main/java/kr/green/practice/service/MemberServiceImp.java
+++ spring/practice/src/main/java/kr/green/practice/service/MemberServiceImp.java
@@ -1,6 +1,7 @@
 package kr.green.practice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.green.practice.dao.MemberDAO;
@@ -11,15 +12,17 @@ public class MemberServiceImp implements MemberService{
 
 	@Autowired
 	MemberDAO memberDao;
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
 	
 	@Override
 	public boolean signup(MemberVO mVo) {
 		if(mVo == null){
 			return false;
 		}
+		mVo.setPw(passwordEncoder.encode(mVo.getPw()));
 		memberDao.signup(mVo);
 		return true;
-		
 	}
 
 	@Override
@@ -28,7 +31,7 @@ public class MemberServiceImp implements MemberService{
 			return null;
 		}
 		MemberVO oVo = memberDao.getMember(mVo.getId());
-		if(oVo.getPw().equals(mVo.getPw())){
+		if(passwordEncoder.matches(mVo.getPw(), oVo.getPw())){
 			return oVo;
 		}
 		return null;
@@ -40,9 +43,10 @@ public class MemberServiceImp implements MemberService{
 			return false;
 		}
 		MemberVO oVo = memberDao.getMember(mVo.getId());
-		if(!oVo.getPw().equals(oPw)){
+		if(!passwordEncoder.matches(oPw, oVo.getPw())){
 			return false;
 		}
+		mVo.setPw(passwordEncoder.encode(mVo.getPw()));
 		memberDao.modify(mVo);
 		return true;
 	}
